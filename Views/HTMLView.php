f775d596196fb7534946c87b499da3e062ae545d
Alex Driaguine <ad222kr@student.lnu.se>
2015-12-12 18:18:53 +0000

    push

--- Views/HTMLView.php
+++ Views/HTMLView.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 2015-12-10
- * Time: 15:02
- */
 class HTMLView {
     public function render() {
         echo
@@ -24,40 +18,39 @@ class HTMLView {
 
     <body>
         <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Trafikinformation</a>
+                </div>
 
-          <a class="navbar-brand" href="#">Trafikinformation</a>
-        </div>
-
-      </div>
-    </nav>
+            </div>
+        </nav>
 
 
         <div class="container">
-        <div class="cont">
-          <div class="map"></div>
-              <div class="row">
-                   <div class="col-md-8 map-area"><div id="map"></div></div>
-                   <div class="col-md-4 info-area">
-                       <div class="form-horizontal">
-                           <div class="form-group">
-                              <label for="categories" class="col-sm-4 control-label">Välj kategori</label>
-                              <div class="col-sm-8">
-                                  <select id = "categories" class="dropdown form-control">
-                                     <option selected="selected" value="4">Alla</option>
-                                     <option value = "0">Vägtrafik</option>
-                                     <option value = "1">Kollektivtrafik</option>
-                                     <option value = "2">Planerad störning</option>
-                                     <option value = "3">Övrigt</option>
-                                  </select>
-                              </div>
-                          </div>
+            <div class="cont">
+                <div class="map"></div>
+                    <div class="row">
+                    <div class="col-md-8 map-area"><div id="map"></div></div>
+                        <div class="col-md-4 info-area">
+                            <div class="form-horizontal">
+                                <div class="form-group">
+                                    <label for="categories" class="col-sm-4 control-label">Välj kategori</label>
+                                        <div class="col-sm-8">
+                                          <select id = "categories" class="dropdown form-control">
+                                             <option selected="selected" value="4">Alla</option>
+                                             <option value = "0">Vägtrafik</option>
+                                             <option value = "1">Kollektivtrafik</option>
+                                             <option value = "2">Planerad störning</option>
+                                             <option value = "3">Övrigt</option>
+                                          </select>
+                                         </div>
+                                </div>
                           <div class="information">
-                          </div>
                       </div>
-                   </div>
-              </div>
+                  </div>
+               </div>
+          </div>
 </div>
         </div>
 
